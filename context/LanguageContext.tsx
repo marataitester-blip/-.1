99695318b56f01523737db2ab7233166a5d22303
marataitester-blip <marat.aitester@gmail.com>
2025-10-19 19:01:57 +0300

    feat: Set default language to Russian
    Changes the default language in the LanguageContext from English to Russian.

--- context/LanguageContext.tsx
+++ context/LanguageContext.tsx
@@ -15,7 +15,7 @@ interface LanguageProviderProps {
 }
 
 export const LanguageProvider: React.FC<LanguageProviderProps> = ({ children }) => {
-  const [language, setLanguage] = useState<Language>('en');
+  const [language, setLanguage] = useState<Language>('ru');
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
